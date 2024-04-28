6c5487d4a55487b60e6bdcc48c075d4e9d81293f
Deepak Arora <arroadeepak0817@gmail.com>
2024-04-28 15:33:21 +0530

    updated package.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,12 +35,11 @@ app.use(cors(
 app.use("/api/auth",authRoutes)
 app.use("/api/tutor", tutorRoutes)
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
 app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
 })
-
 
 
 app.listen('7000',()=>{
